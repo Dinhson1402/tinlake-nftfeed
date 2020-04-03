fff17f4b1353da21c299a3dfd7faf6d5a419693a
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2020-04-03 12:21:56 +0200

    added constructor

--- src/test/nftfeed.t.sol
+++ src/test/nftfeed.t.sol
@@ -13,16 +13,26 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity >=0.5.3;
+pragma solidity >=0.5.15;
 
 import "ds-test/test.sol";
+
+import "./../nftfeed.sol";
 
 
 contract NFTFeedTest is DSTest {
 
+    NFTFeed public nftFeed;
 
     function setUp() public {
-        // todo
+        // default values
+        uint thresholdRatio = 8*10**26;                     // 80% threshold
+        uint ceilingRatio = 6*10**26;                       // 60% ceiling
+        uint lowRate = uint(1000000564701133626865910626);  // 5 % day
+
+        nftFeed = new NFTFeed();
+
+
     }
 
 

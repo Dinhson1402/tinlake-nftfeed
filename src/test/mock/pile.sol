420b9a1d6b14e16d7247c44d8687ad9fca5e3a95
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2020-04-03 12:43:04 +0200

    added mocks

---
+++ src/test/mock/pile.sol
@@ -0,0 +1,57 @@
+pragma solidity >=0.5.15 <0.6.0;
+
+import "./mock.sol";
+
+contract PileMock is Mock {
+    function total() public view returns(uint) {
+        return values_return["total"];
+    }
+
+    function setRate(uint loan, uint rate) public {
+        values_uint["setRate_loan"] = loan;
+        values_uint["setRate_rate"] = rate;
+        calls["setRate"]++;
+    }
+
+    function changeRate(uint loan, uint rate) public {
+        values_uint["changeRate_loan"] = loan;
+        values_uint["changeRate_rate"] = rate;
+        calls["changeRate"]++;
+
+    }
+
+    function debt(uint loan) public returns(uint) {
+        // name = "debt_loan" because of two debt funcs
+        values_uint["debt_loan_loan"] = loan;
+        calls["debt_loan"]++;
+        return values_return["debt_loan"];
+    }
+
+    function debt() public returns(uint) {
+        return call("debt");
+    }
+
+    function incDebt(uint loan, uint currencyAmount) public {
+        values_uint["incDebt_loan"] = loan;
+        values_uint["incDebt_currencyAmount"] = currencyAmount;
+        calls["incDebt"]++;
+
+    }
+
+    function decDebt(uint loan, uint currencyAmount) public {
+        values_uint["decDebt_loan"] = loan;
+        values_uint["decDebt_currencyAmount"] = currencyAmount;
+        calls["decDebt"]++;
+    }
+
+    function accrue(uint loan) public {
+        values_uint["accrue_loan"] = loan;
+        calls["accrue"]++;
+    }
+
+    function file(uint rate, uint speed) public {
+        values_uint["file_rate"] = rate;
+        values_uint["file_speed"] = speed;
+        calls["file"]++;
+    }
+}

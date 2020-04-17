42658e66b15801e97676bd1950c9482491e62cd4
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2020-04-17 18:44:13 +0200

    added simple nav

---
+++ src/feed.sol
@@ -0,0 +1,62 @@
+// Copyright (C) 2020 Centrifuge
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity >=0.5.15;
+import "ds-note/note.sol";
+import "tinlake-auth/auth.sol";
+import "tinlake-math/math.sol";
+import "ds-test/test.sol";
+import "./nftfeed.sol";
+contract Feed is BaseNFTFeed, DSTest {
+    // dueDate => FV
+    mapping (uint => uint) public futureValueAtDate;
+    // nftID => dueDate
+    mapping (bytes32 => uint) public dueDate;
+    uint public discountRate;
+    uint public maxDays;
+    function uniqueDayTimestamp(uint timestamp) public pure returns (uint) {
+        return (1 days) * (timestamp/(1 days));
+    }
+    /// dueDate is timestamp
+    function file(bytes32 what, bytes32 nftID_, uint dueDate_) public {
+        if (what == "duedate") {
+            dueDate[nftID_] = uniqueDayTimestamp(dueDate_);
+        } else { revert("unknown config parameter");}
+    }
+    constructor (uint discountRate_, uint maxDays_) public {
+        discountRate = discountRate_;
+        maxDays = maxDays_;
+    }
+    // Ceiling Implementation
+    function borrow(uint loan, uint amount) external auth {
+        // ceiling check uses existing loan debt
+        uint normalizedDay = uniqueDayTimestamp(now);
+        require(ceiling(loan) >= safeAdd(pile.debt(loan), amount), "borrow-amount-too-high");
+        bytes32 nftID_ = nftID(loan);
+        // calculate future cash flow
+        futureValueAtDate[dueDate[nftID_]] = rmul(rpow(loanRatePerSecond(loan),  dueDate[nftID_]  - normalizedDay, ONE), amount);
+    }
+
+    function repay(uint loan, uint amount) external auth {}
+
+    function nav() public view returns(uint) {
+        uint normalizedDay = uniqueDayTimestamp(now);
+        uint sum = 0;
+
+        // current implementation ignores overdue nfts
+        for (uint i = 0;i <= maxDays; i=i + 1 days) {
+            sum += rdiv(futureValueAtDate[normalizedDay + i], rpow(discountRate,  i, ONE));
+        }
+        return sum;
+    }
+}
---
+++ src/test/feed.t.sol
@@ -0,0 +1,88 @@
+// Copyright (C) 2020 Centrifuge
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity >=0.5.15;
+import "ds-test/test.sol";
+import "./../feed.sol";
+import "./mock/shelf.sol";
+import "./mock/pile.sol";
+contract Hevm {
+    function warp(uint256) public;
+}
+contract NAVTest is DSTest {
+    Feed public feed;
+    ShelfMock shelf;
+    PileMock pile;
+    uint defaultRate;
+    uint defaultThresholdRatio;
+    uint defaultCeilingRatio;
+    uint discountRate;
+    Hevm hevm;
+    function setUp() public {
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        hevm.warp(1234567);
+        // default values
+        defaultThresholdRatio = 8*10**26;                     // 80% threshold
+        defaultCeilingRatio = 6*10**26;                       // 60% ceiling
+        defaultRate = uint(1000000564701133626865910626);     // 5 % day
+        discountRate = uint(1000000342100000000000000000);    // 3 % day
+        uint maxDays = 10 days;
+        uint defaultRisk = 0;
+        feed = new Feed(discountRate, maxDays);
+        pile = new PileMock();
+        shelf = new ShelfMock();
+        feed.depend("shelf", address(shelf));
+        feed.depend("pile", address(pile));
+        feed.setRiskGroup(defaultRisk, defaultThresholdRatio, defaultCeilingRatio, defaultRate);
+    }
+    function prepareDefaultNFT(uint nftValue) public returns(bytes32, uint) {
+        bytes32 nftID = feed.nftID(address(1), 1);
+        feed.update(nftID, nftValue);
+        uint loan = 1;
+        shelf.setReturn("shelf",address(1), 1);
+        pile.setReturn("debt_loan", 0);
+        pile.setReturn("rates_ratePerSecond", defaultRate);
+        return (nftID, loan);
+    }
+    function testSimpleBorrow() public {
+        uint value = 100 ether;
+        (bytes32 nftID, uint loan) = prepareDefaultNFT(value);
+        uint dueDate = now + 2 days;
+        feed.file("duedate",nftID, dueDate);
+        uint amount = 50 ether;
+        feed.borrow(loan, amount);
+
+        // check FV
+        uint normalizedDueDate = feed.uniqueDayTimestamp(dueDate);
+
+        uint FV = 55.125 ether; // 50 * 1.05 ^ 2 = 55.125
+        assertEq(feed.futureValueAtDate(normalizedDueDate), FV);
+        // FV/(1.03^2)
+        assertEq(feed.nav(), 51.960741582371777180 ether);
+        hevm.warp(now + 1 days);
+        // FV/(1.03^1)
+        assertEq(feed.nav(), 53.519490652735515520 ether);
+        hevm.warp(now + 1 days);
+        // FV/(1.03^0)
+        assertEq(feed.nav(), 55.125 ether);
+    }
+    function testNormalizeDate() public {
+        uint randomUnixTimestamp = 1586977096; // 04/15/2020 @ 6:58pm (UTC)
+        uint dayTimestamp = feed.uniqueDayTimestamp(randomUnixTimestamp);
+        assertTrue(feed.uniqueDayTimestamp(randomUnixTimestamp) != randomUnixTimestamp);
+        uint delta = randomUnixTimestamp - dayTimestamp;
+        assertTrue(delta < 24*60*60);
+        randomUnixTimestamp += 3 hours;
+        assertTrue(feed.uniqueDayTimestamp(randomUnixTimestamp) == dayTimestamp);
+    }
+}
